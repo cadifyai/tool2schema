deb7bf7871277fffbfbf34f7be66fd57f0b5ebf7
Angus Stewart <siliconlad@protonmail.com>
2024-01-26 12:50:59 +0000

    Support only python39

--- tests/functions.py
+++ tests/functions.py
@@ -1,4 +1,3 @@
-from __future__ import annotations
 from tool2schema import GPTEnabled
 
 
--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,4 +1,3 @@
-from __future__ import annotations
 from typing import List, Optional
 
 from tool2schema import (
